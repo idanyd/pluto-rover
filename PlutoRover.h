fec5e3da31bf840a102ea3a3f5ea69df0dcdb56b
idanyd <44985669+idanyd@users.noreply.github.com>
2018-11-17 18:46:51 +0000

    Added grid

--- PlutoRover.h
+++ PlutoRover.h
@@ -16,9 +16,13 @@ public:
 		unsigned long m_y;
 		char m_heading;
 	};
-
+	
 public:
-	PlutoRover() : m_position(0, 0, 'N') {}
+	// A rover must know the size of the grid on the planet it's on. 
+	// Please provide the grid's size (x and y coordinates)
+	PlutoRover(unsigned long grid_x, unsigned long grid_y) : 
+		m_position(0, 0, 'N'), m_grid_x(grid_x), m_grid_y(grid_y) {}
+
 	virtual ~PlutoRover() {}
 
 	// Use this method to get the rover's current position
@@ -42,4 +46,6 @@ private:
 
 private:
 	Position m_position;
+	unsigned long m_grid_x;
+	unsigned long m_grid_y;
 };
