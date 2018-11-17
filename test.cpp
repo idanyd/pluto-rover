f7dfbd511f609ef4c0d2df68b714c808a5538cc9
idanyd <44985669+idanyd@users.noreply.github.com>
2018-11-17 18:28:12 +0000

    Additional tests

--- test.cpp
+++ test.cpp
@@ -22,3 +22,41 @@ TEST(PlutoRoverTest, TestForwardCommand) {
 
 	ASSERT_EQ(position, rover.GetPos());
 }
+
+TEST(PlutoRoverTest, TestSimpleCommand) {
+
+	PlutoRover rover;
+
+	PlutoRover::Position position(2, 2, 'E');
+
+	std::string commands("FFRFF");
+	rover.Go(commands);
+
+	ASSERT_EQ(position, rover.GetPos());
+}
+
+TEST(PlutoRoverTest, TestSimpleCommandWithUnhandledcharacters) {
+
+	PlutoRover rover;
+
+	PlutoRover::Position position(2, 2, 'E');
+
+	std::string commands("Ff2F7jR<>qF88888888kljahdf*F+*/!£$%"); // Same as "FFRFF"
+	rover.Go(commands);
+
+	ASSERT_EQ(position, rover.GetPos());
+}
+
+TEST(PlutoRoverTest, TestChainedCommands) {
+
+	PlutoRover rover;
+
+	PlutoRover::Position position(1, 5, 'N');
+
+	std::string commands1("FFRFF");
+	std::string commands2("LLFRFFF");
+	rover.Go(commands1);
+	rover.Go(commands2);
+
+	ASSERT_EQ(position, rover.GetPos());
+}
