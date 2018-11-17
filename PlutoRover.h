7808f424032eb36ca87387d5e245069691352be1
idanyd <44985669+idanyd@users.noreply.github.com>
2018-11-17 18:09:37 +0000

    Added to Go() command
    The rover can now handle commands.

--- PlutoRover.h
+++ PlutoRover.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 class PlutoRover
 {
 public:
@@ -19,10 +21,25 @@ public:
 	PlutoRover() : m_position(0, 0, 'N') {}
 	virtual ~PlutoRover() {}
 
+	// Use this method to get the rover's current position
+	// The returned format is (x, y, heading)
 	Position GetPos() { return m_position; }
+
+	// Use this method to give the rover commands. 
+	// The commands are:
+	// 'F' - move forward by 1 square
+	// 'B' - move back by 1 square
+	// 'L' - Turn 90 degrees left
+	// 'L' - Turn 90 degrees right
+	// Any other character will be ignored by the rover
+	void Go(const std::string& commands);
+
+private:
+	void GoForward();
+	void GoBack();
+	void TurnLeft();
+	void TurnRight();
 
 private:
 	Position m_position;
 };
-
-
